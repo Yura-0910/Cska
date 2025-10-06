17d1fccaf1f80bac327aa5d6ae13a33224717e70
Yura Mazurov <mazurovyura09@yandex.ru>
2025-10-06 16:13:23 +0300

    Добавил вывод времени

--- Timer.py
+++ Timer.py
@@ -4,16 +4,19 @@
 from tkinter import messagebox
 import tkinter as tk
 import time
+from datetime import datetime
 
 def timer():
    root = tk.Tk()
    root.withdraw()
    messagebox.showinfo("Вопрос", "Есть ли биллеты на 21-12-2025 ?")
    root.destroy()
+   current_datetime = datetime.now()
+   print(f"Текущая дата и время: {current_datetime}")
 
 while True:
    timer()
-   time.sleep(5)
+   time.sleep(3600)
        
 #Звуковой сигнал проигровать
 
