33cc9dc44af67f70fe55ec80d42fd6d200224dca
Yura Mazurov <mazurovyura09@yandex.ru>
2025-10-07 17:40:27 +0300

    Добавил звуковой сигнал

--- Timer.py
+++ Timer.py
@@ -9,6 +9,7 @@ from datetime import datetime
 def timer():
    root = tk.Tk()
    root.withdraw()
+   print("\a", end='', flush=True)
    messagebox.showinfo("Вопрос", "Есть ли биллеты на 21-12-2025 ?")
    root.destroy()
    current_datetime = datetime.now()
@@ -18,7 +19,4 @@ while True:
    timer()
    time.sleep(3600)
        
-#Звуковой сигнал проигровать
-
-
-
+#Звуковой сигнал проигровать:: три раза, с паузой в секунду. Затем выдать сообщение.
