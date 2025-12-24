896bca64f6465280d3f6b78a6e5f66241b255137
Yura Mazurov <mazurovyura09@yandex.ru>
2025-12-24 16:42:53 +0300

    Добавил часы, минуты

--- Date.py
+++ Date.py
@@ -3,17 +3,22 @@
 
 from datetime import datetime, date, timedelta
 def raznica1():
-   datetime1 = datetime(2025, 9, 25)
-   datetime2 = datetime(2025, 10, 26)
-   difference = (datetime2 - datetime1).days
-   print(f"Разница между {datetime1} и {datetime2} будет {difference} дня(ей)") 
+   datetime1 = datetime(2025, 12, 24, 15, 50, 0) #24 декабря 2025 15:50
+   datetime2 = datetime(2026, 1, 11, 18, 0, 0)  
+   diff = (datetime2 - datetime1)
+   days = diff.days
+   #где 86400 = 24 часа × 60 минут × 60 секунд
+   #где /3600 - для перевода в часы (60 сек * 60 минут)
+   hours = int((diff.total_seconds() - days*86400)/3600)
+   minutes = int((diff.total_seconds() - days*86400 - hours*3600)/60)  
+   print(f"Разница между {datetime1} и {datetime2}")
+   print(f"будет {days} дня(ей) {hours} часов {minutes} минут") 
 
 def raznica2():
    original_date = date(2025, 12, 21)
    print(f"Дата начала матча: {original_date}")
-   new_date = original_date - timedelta(days=31)
-   print(f"За 31 день до начала матча это дата:: {new_date}")
+   new_date = original_date - timedelta(days=41)
+   print(f"За 41 день до начала матча это дата:: {new_date}")
 
 raznica1()
 raznica2()
-
